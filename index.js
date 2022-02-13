28cbf934540d836bbfac4be779f7f3f561b5ba21
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-02-12 21:56:42 -0500

    commit tree 456aafe78b0632b0d35988ef31f0dc8412fc46f6

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code here
+
+// //Review 
+// class Dog {
+//     constructor(name, breed) {
+//         this.name = name;
+//         this.breed = breed;
+//     }
+// }
+// let dog = new Dog('Ted', 'corgi');
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+
